527b793bd541994121cbcb00b021c1b66c9ab85b
Andre Toerien <49614525+AThePeanut4@users.noreply.github.com>
2023-11-10 21:14:32 +0200

    Fix LSP position params

--- lua/hover/providers/lsp.lua
+++ lua/hover/providers/lsp.lua
@@ -14,18 +14,27 @@ require('hover').register {
     return false
   end,
   execute = function(opts, done)
+    local util = require('vim.lsp.util')
+
+    local row, col = unpack(opts.pos)
+    local offset_encoding = util._get_offset_encoding(opts.bufnr)
+    row = row - 1
+    local line = vim.api.nvim_buf_get_lines(opts.bufnr, row, row + 1, true)[1]
+    if line then
+      col = util._str_utfindex_enc(line, col, offset_encoding)
+    end
+
     local params = {
-      textDocument = { uri = vim.uri_from_bufnr(opts.bufnr) },
+      textDocument = util.make_text_document_params(opts.bufnr),
       position = {
-        line = opts.pos[1],
-        character = opts.pos[2]
+        line = row,
+        character = col,
       }
     }
 
     vim.lsp.buf_request_all(0, 'textDocument/hover', params, function(responses)
       for _, response in pairs(responses) do
         if response.result and response.result.contents then
-          local util = require('vim.lsp.util')
           local lines = util.convert_input_to_markdown_lines(response.result.contents)
 
           if not vim.tbl_isempty(lines) then
