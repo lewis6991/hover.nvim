222b579ead974f489a60b331b106e5fbb42903fb
Ahmed El Gabri <ahmed@gabri.me>
2024-05-21 17:09:16 +0200

    Add comment to describe the pattern matching

--- lua/hover/providers/gh_user.lua
+++ lua/hover/providers/gh_user.lua
@@ -5,6 +5,8 @@ local async = require('hover.async')
 ---@return string
 local function get_user()
   local WORD = fn.expand('<cWORD>')
+  -- The regex is not perfect, but it should match @user and user
+  -- The %f[%w_] and %f[^%w_] is because of the lack of \b in Lua. https://stackoverflow.com/a/32854326/213124
   local user = WORD:match '%f[%w_]@?(.*)%f[^%w_]'
   return user
 end
