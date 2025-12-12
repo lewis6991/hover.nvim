e61604205336d4461d2f910ecbb9f2ddc0b947a0
Lewis Russell <lewis6991@gmail.com>
2025-12-12 12:06:22 +0000

    feat: improve jira provider

--- lua/hover/providers/jira.lua
+++ lua/hover/providers/jira.lua
@@ -1,29 +1,32 @@
+-- Match 2 or more uppercase letters followed by a '-' and 1 or more digits.
 local ISSUE_PATTERN = '%u[%u%d]+-%d+'
+
+--- @param str string
+--- @return string
+local function strip_ansi_codes(str)
+  return (str:gsub('\27%[[%d;]*m', ''))
+end
 
 --- @type Hover.Provider
 return {
   name = 'Jira',
   priority = 175,
   enabled = function()
-    -- Match 2 or more uppercase letters followed by a '-' and 1 or more digits.
-    return vim.fn.expand('<cWORD>'):match(ISSUE_PATTERN) ~= nil
+    return vim.fn.executable('jira') == 1 and vim.fn.expand('<cWORD>'):match(ISSUE_PATTERN) ~= nil
   end,
   execute = function(_params, done)
     local query = vim.fn.expand('<cWORD>'):match(ISSUE_PATTERN)
 
     vim.system({ 'jira', 'issue', 'view', query, '--plain' }, {}, function(result)
-      if not result.code > 0 then
-        done(false)
+      if result.code > 0 then
+        local stripped_stderr = strip_ansi_codes(result.stderr or '')
+        done({ lines = vim.split(stripped_stderr, '\n'), filetype = 'text' })
         return
       end
 
-      local lines = {}
+      local lines = {} --- @type string[]
       for line in assert(result.stdout):gmatch('[^\r\n]+') do
-        -- Remove lines starting with \27, which is not formatted well and
-        -- is only there for help/context/suggestion lines anyway.
-        if line:find('^\27') == nil then
-          table.insert(lines, line)
-        end
+        lines[#lines + 1] = strip_ansi_codes(line)
       end
 
       done({ lines = lines, filetype = 'markdown' })
